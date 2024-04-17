6b3eb44832477585a30e559dc0c26243fff14853
Akash Saxena <iiakash59@gmail.com>
2024-04-17 17:36:20 +0530

    Update file9.c

--- Wipro/Assignment/assignment/day05/file9.c
+++ Wipro/Assignment/assignment/day05/file9.c
@@ -1,3 +1,27 @@
+/*Problem
+statement:Write the logic to print star pattern or rectangle star pattern.
+Input
+Format  : First line contains N of type
+integer.
+Output
+Format : Print the rectangle star pattern.
+Constrains:
+
+2<=N<=10
+
+Sample Input:
+5
+
+Sample Output:
+*****
+*****
+*****
+*****
+*****
+
+    */
+
+
 #include <stdio.h>
 
 int main(){
